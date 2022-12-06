443a83d447aa8cc5e77287c6b1eaa3c7f80b13a1
eggy <eggyrules@gmail.com>
2022-12-06 15:27:03 -0500

    fix day 6

--- src/day6-1.rs
+++ src/day6-1.rs
@@ -7,7 +7,7 @@ fn main() {
     stdin().read_line(&mut line).unwrap();
 
     for i in 4..line.len() {
-        if HashSet::<char>::from_iter(line[i - 4..i].chars()).len() == 4 {
+        if HashSet::<char>::from_iter(line[i - 4..i]).len() == 4 {
             println!("{}", i);
             break;
         }
