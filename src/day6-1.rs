8eb7ad4eae3550752518fad3935e6c45548458c9
eggy <eggyrules@gmail.com>
2022-12-06 15:26:44 -0500

    fix day 6

--- src/day6-1.rs
+++ src/day6-1.rs
@@ -1,30 +1,13 @@
+use std::collections::HashSet;
 use std::io::stdin;
-
-fn no_dupes(slice: &str) -> bool {
-    for (i, c) in slice.chars().enumerate() {
-        for (j, c2) in slice.chars().enumerate() {
-            if i == j {
-                continue;
-            }
-
-            if c == c2 {
-                return false;
-            }
-        }
-    }
-    true
-}
 
 fn main() {
     let mut line = String::new();
 
     stdin().read_line(&mut line).unwrap();
 
-    for i in 0..line.len() {
-        if i < 4 {
-            continue;
-        }
-        if no_dupes(&line[i - 4..i]) {
+    for i in 4..line.len() {
+        if HashSet::<char>::from_iter(line[i - 4..i].chars()).len() == 4 {
             println!("{}", i);
             break;
         }
