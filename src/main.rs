f2a6e19b44dcbebee9d406720c1d4061e261bf21
eggy <eggyrules@gmail.com>
2022-12-15 20:41:52 -0500

    fix day 14

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io::stdin;
 
-const BOUNDS: (i32, i32) = (0, 10000 + 1);
+const BOUNDS: (i32, i32) = (0, 4000000 + 1);
 
 struct Sensor {
     pos: (i32, i32),
@@ -22,8 +22,6 @@ fn main() {
     let mut buf = String::new();
     let mut sensors: Vec<Sensor> = vec![];
 
-    let mut max = (0, 0);
-    let mut min = (0, 0);
     while stdin().read_line(&mut buf).unwrap() > 1 {
         let process = buf
             .trim()
@@ -33,54 +31,44 @@ fn main() {
 
         let sen = Sensor::new(process[0], process[1], process[2], process[3]);
 
-        max.0 = std::cmp::max(max.0, sen.pos.0 + sen.beacon_distance);
-        max.1 = std::cmp::max(max.1, sen.pos.1 + sen.beacon_distance);
-        min.0 = std::cmp::min(min.0, sen.pos.0 - sen.beacon_distance);
-        min.1 = std::cmp::min(min.1, sen.pos.1 - sen.beacon_distance);
-
         sensors.push(sen);
         buf.clear();
     }
 
     for dest in BOUNDS.0..BOUNDS.1 {
-        if dest % 1000 == 0 {
-            println!("{} / {}", dest, BOUNDS.1);
-        }
-        let mut row = vec!['.'; (max.0 - min.0 + 1) as usize];
+        let mut ranges: Vec<(i32, i32)> = vec![];
         for sen in &sensors {
-            let x = sen.pos.0 - min.0;
-            let y = sen.pos.1 - min.1;
-            let d = sen.beacon_distance;
-            let dest_rel = dest - min.1;
+            let (x, y) = sen.pos;
+            let dest_rel = (dest - x).abs();
 
-            if y + d >= dest_rel && y - d <= dest_rel {
-                for i in 0..d - (dest_rel - y).abs() + 1 {
-                    row[(x + i) as usize] = '#';
-                    row[(x - i) as usize] = '#';
-                }
+            if dest_rel <= sen.beacon_distance {
+                let remainder = (sen.beacon_distance - dest_rel).abs();
+                ranges.push((y - remainder, y + remainder));
             }
         }
 
-        for sen in &sensors {
-            let x = sen.pos.0 - min.0;
-            let y = sen.pos.1 - min.1;
-
-            let dest_rel = dest - min.1;
-            if y == dest_rel {
-                row[x as usize] = 'S';
-            }
-
-            if sen.beacon.1 == dest {
-                row[(sen.beacon.0 - min.0) as usize] = 'B';
-            }
-        }
-
-        if let Some(i) = row[(BOUNDS.0 - min.0) as usize..(BOUNDS.1 - min.0) as usize]
-            .iter()
-            .position(|&c| c == '.')
-        {
-            let freq = i as i32 * 4000000 + dest;
+        ranges.sort();
+        let mut highest = 0;
+        if ranges[0].0 > BOUNDS.0 {
+            let freq = dest * 4000000 + (ranges[0].0 - 1);
             println!("{}", freq);
+            return;
+        }
+
+        for &(start, end) in ranges.iter() {
+            if start > highest {
+                println!("{} {}", start, end);
+                let freq: i64 = dest as i64 * 4000000 + (start as i64 - 1);
+                println!("{}", freq);
+                return;
+            }
+            highest = std::cmp::max(highest, end + 1);
+        }
+
+        if highest < BOUNDS.1 {
+            let freq = dest * 4000000 + highest;
+            println!("{}", freq);
+            return;
         }
     }
 }
